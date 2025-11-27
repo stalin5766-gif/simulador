f7c3f506ad99bfff7c08849e2da66eb86b2bfdd0
stalin5766-gif <stalin5766@gmail.com>
2025-11-27 11:08:49 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,13 +2,8 @@
 // 1. LISTA BLANCA (Correos Autorizados)
 // ==========================================
 const correosPermitidos = [
-  "dpachecog2@unemi.edu.ec", "cnavarretem4@unemi.edu.ec", "htigrer@unemi.edu.ec",
-  "gorellanas2@unemi.edu.ec", "iastudillol@unemi.edu.ec", "sgavilanezp2@unemi.edu.ec",
-  "jzamoram9@unemi.edu.ec", "fcarrillop@unemi.edu.ec", "naguilarb@unemi.edu.ec",
-  "ehidalgoc4@unemi.edu.ec", "lbrionesg3@unemi.edu.ec", "xsalvadorv@unemi.edu.ec",
-  "nbravop4@unemi.edu.ec", "jmoreirap6@unemi.edu.ec", "kholguinb2@unemi.edu.ec",
-  "jcastrof8@unemi.edu.ec", "cgomezy2@unemi.edu.ec", "mvegaa2@unemi.edu.ec", "earrobas@unemi.edu.ec",
-  "ccedenov6@unemi.edu.ec", "dzambranod6@unemi.edu.ec", // <-- Aquí terminan los UNEMI
+  "cgomezy2@unemi.edu.ec", "mvegaa2@unemi.edu.ec", "earrobas@unemi.edu.ec",
+  "ccedenov6@unemi.edu.ec", "dzambranod6@unemi.edu.ec", "sgavilanezp2@unemi.edu.ec" // <-- Aquí terminan los UNEMI
 
   // Gmail añadido (Separados por coma dentro del array)
   "apoyochat.trabajosocial@gmail.com", 
